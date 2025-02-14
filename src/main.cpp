10eb24d44e4d5697e54090a752455e694421f516
PierreYvesMal <malengre.py@gmail.com>
2025-02-14 22:04:19 +0100

    12: WIP not even printing from main

--- src/main.cpp
+++ src/main.cpp
@@ -13,32 +13,68 @@
 
 #include "EasyCAT.h"
 
+EasyCAT EASYCAT; 
+
 int main()
 {
     std::cout << "\n\nPrint from main" << std::endl;
+
+    if(!EASYCAT.Init())
+    {
+        std::cout << "Easycat failed to init" << std::endl;
+        printf("Init failed \n");
+    }
+    else
+    {
+        std::cout << "Easy cat initialized" << std::endl;
+        printf("Init succesful \n");
+    }
 
     EventQueue EVqueue;
     Thread eventThread;
     eventThread.start(callback(&EVqueue, &EventQueue::dispatch_forever));
 
     auto robot = new Robot(&EVqueue);
-    //robot->InitializeMotorbase();
-    //robot->Calibrate();
-    //robot->SetACC(0x0100);
-    //robot->SetACC(0x11e);
-    //robot->SetPID(0x0650, 0x1, 0x250);
 
     double speed = 100;
     robot->Move(1000, 0, 0, speed, 0);
-    // wait_us(1000000);
-    // robot->Move(1000, 90, 0, speed, 0);
-    // wait_us(1000000);
-    // robot->Move(0, 0, 360, speed, 0);
 
-    // Should move for 10s
-    //robot->Move(500, PI/2, 0, 100, 0);
-    //robot->Move(500, 0, 0, 100, 0);
-    //robot->Calibrate();
+    while(true)
+    {
+        uint8_t status = static_cast<uint8_t>(EASYCAT.MainTask() & 0x7f);
+        if(status < 4)
+        {
+            std::cout << "Incorrect status: " << status << std::endl;
+        }
 
+        static int count = 0;
+        if(count % 100 == 0)
+        {
+            printf("wanted distance MSB: %02x\n",EASYCAT.BufferOut.Byte[0]);
+            printf("wanted distance: LSB: %02x\n",EASYCAT.BufferOut.Byte[1]);
+            printf("wanted angle MSB: %02x\n",EASYCAT.BufferOut.Byte[2]);
+            printf("wanted angle LSB: %02x\n",EASYCAT.BufferOut.Byte[3]);
+        }
+
+        EASYCAT.BufferIn.Byte[0]=0x01;
+        EASYCAT.BufferIn.Byte[1]=0x02;
+        EASYCAT.BufferIn.Byte[2]=0x03; 
+    
+        uint16_t wanted_speed = ((EASYCAT.BufferOut.Byte[0] << 8) & 0xF0) | EASYCAT.BufferOut.Byte[1];
+        printf("wanted_speed %02x\n", wanted_speed);
+        //ThisThread::sleep_for(100);
+        if(EASYCAT.BufferOut.Byte[1] % 2 == 0)
+        {
+            //robot->Move(wanted_speed, 0, 0, 1, 32);
+        }
+        else
+        {
+            //robot->Move(wanted_speed, 180, 0, 1, 32);
+        }
+        
+        EVqueue.dispatch_once();
+        EVqueue.dispatch_once();
+        EVqueue.dispatch_once();
+    }
     EVqueue.dispatch_forever();    
 };
